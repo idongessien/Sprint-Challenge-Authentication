c9525449ca129a46a085aa3e226342bfb74d874b
Idong Essien <idong.essien@comcast.net>
2020-04-24 14:18:41 -0400

    spec file modified more tests

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,11 +1,33 @@
-const server = require("../api/server")
- const request = require("supertest")
- const db = require("../database/dbConfig")
+const Auth = require('./auth-model.js');
+const db = require('../database/dbConfig.js');
+const server = require('../api/server');
+const request = require('supertest');
 
- describe('post /', function(){
-     it('should return 200 OK ', async function(){
-         const response = await request(server).post("/api/auth/register")
-         expect(response.status).toBe(201)
-        
-     })
- })
+describe('Auth', () => {
+    beforeEach(async () => {
+        await db('users').truncate();
+    })
+
+    it('should set the environment to testing', () => {
+        expect(process.env.DB_ENV).toBe('testing')
+    });
+
+    describe('add()', () => {
+        it('should add user to db', async () => {
+            await Auth.add({ username: "Idong", password: 'password' });
+            let user = await db('users');
+            expect(user).toHaveLength(1);
+        })
+    })
+})
+
+describe('POST login /', () => {
+    it('returns 401 OK with invalid credentials', () => {
+        return (
+            request(server)
+                .post('/api/auth/login')
+                .send({username: "user", password: 'password'})
+                .expect(401)
+        )
+    })
+})
